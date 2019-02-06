67d2bbcaa43236484d17433b774db265ebd019d4
Anthea Gonzalez <agonzalez2@hawk.itt.edu>
2019-02-06 16:32:30 -0600

    Final class adds

--- site.js
+++ site.js
@@ -1,13 +1,36 @@
-
-
+(funciton()){
 
 if(!('querySelector' in document && 'addEventListener' in document)) {
   concole.log('Old brouser');
   return;
 }
 
+var phone_submit = document.querySelector('#signup');
+
 document.addEventListener('DOMContentLoaded', function(){
-  console.log('Dom loaed');
-  var heading_text = document.querySelector('#content h1').innerText();
-  console.log('heading test is:', heading_text);
-})
+//  console.log('Dom loaed');
+//  var heading_text = document.querySelector('#content h1').innerText();
+//  console.log('heading test is:', heading_text);
+
+phone_submit.setAttribute('disabled','disabled');
+});
+
+var tel_input = document.querySelector('#telephone');
+tel_input.addEventListener('focus', funciton(){
+  concole.log('focus');
+});
+tel_input.addEventListener('blur', funciton(){
+  concole.log('not focus');
+});
+tel_input.addEventListener('keyup', funciton(){
+  concole.log('value', this.value);
+  var clean_number = this.value.replace(/\D/g,'');
+  var sanitized_number = clean_number.replace(/^1/,'');
+  if (sanitized_number.length === 10) {
+    console.log('awesome sause b')
+    phone_submit.removeAttribute('disabled')
+  }
+
+});
+
+}());
