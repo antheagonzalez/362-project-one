0e36da4480642bed5cc10d27ddb8ff998322ddad
Anthea Gonzalez <agonzalez2@hawk.itt.edu>
2019-02-06 23:03:46 -0600

    Update js comments

--- site.js
+++ site.js
@@ -1,36 +1,37 @@
-(funciton()){
-
-if(!('querySelector' in document && 'addEventListener' in document)) {
-  concole.log('Old brouser');
-  return;
-}
-
-var phone_submit = document.querySelector('#signup');
-
-document.addEventListener('DOMContentLoaded', function(){
-//  console.log('Dom loaed');
-//  var heading_text = document.querySelector('#content h1').innerText();
-//  console.log('heading test is:', heading_text);
-
-phone_submit.setAttribute('disabled','disabled');
-});
-
-var tel_input = document.querySelector('#telephone');
-tel_input.addEventListener('focus', funciton(){
-  concole.log('focus');
-});
-tel_input.addEventListener('blur', funciton(){
-  concole.log('not focus');
-});
-tel_input.addEventListener('keyup', funciton(){
-  concole.log('value', this.value);
-  var clean_number = this.value.replace(/\D/g,'');
-  var sanitized_number = clean_number.replace(/^1/,'');
-  if (sanitized_number.length === 10) {
-    console.log('awesome sause b')
-    phone_submit.removeAttribute('disabled')
+(funciton(){
+// Browser sanity check
+  if(!('querySelector' in document && 'addEventListener' in document)) {
+    concole.log('Old browser');
+    return;
   }
 
+  var phone_submit = document.querySelector('#signup');
+
+  document.addEventListener('DOMContentLoaded', function(){
+    //  console.log('Dom loaed');
+    //  var heading_text = document.querySelector('#content h1').innerText();
+    //  console.log('heading test is:', heading_text);
+    phone_submit.setAttribute('disabled','disabled');
 });
+
+  var tel_input = document.querySelector('#telephone');
+  tel_input.addEventListener('focus', funciton(){
+    concole.log('Focusing on telephone input');
+});
+  tel_input.addEventListener('blur', funciton(){
+    concole.log('Not focusing on telephone input');
+});
+  tel_input.addEventListener('keyup', funciton(){
+    // Remove non digit characters
+    var clean_number = this.value.replace(/\D/g,'');
+    // Remove any 1's at the beginning
+    var sanitized_number = clean_number.replace(/^1/,'');
+    // Check to see if number is at least 10 digits long
+    if (sanitized_number.length === 10) {
+      console.log('Number is valid.');
+      phone_submit.removeAttribute('disabled');
+    }
+
+  });
 
 }());
